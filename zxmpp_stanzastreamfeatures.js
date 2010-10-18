b3dfec5cbc8ec884eb325a80e137e5b84dfc43ff
Ivan Vučica <ivan@vucica.net>
2010-10-18 02:47:21 +0200

    Sending PLAIN auth

--- zxmpp_stanzastreamfeatures.js
+++ zxmpp_stanzastreamfeatures.js
@@ -12,6 +12,32 @@ zxmppClass.prototype.stanzaStreamFeatures = function(zxmpp)
 	
 	this.parseXML = function (xml)
 	{
-		
+		for(var i in xml.childNodes)
+		{
+			var child = xml.childNodes[i];
+			if(child && child.nodeName)
+			{
+				this.zxmpp.util.easierAttrs(child);
+				if(child.nodeName == "mechanisms" && child.attr["xmlns"]=="urn:ietf:params:xml:ns:xmpp-sasl")
+				{
+					this.parseMechanisms(child);
+				}
+				
+			}
+		}
 	}
+	
+	
+	this.parseMechanisms = function(xml)
+	{
+		for(var i in xml.childNodes)
+		{
+			var child = xml.childNodes[i];
+			if(child.nodeName=="mechanism")
+			{
+				this.zxmpp.saslMechanisms[child.firstChild.nodeValue.toUpperCase()] = true;
+			}
+		}
+	}
+	
 }
