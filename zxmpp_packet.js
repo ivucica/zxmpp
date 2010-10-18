66ba2f1ad441fde3f3d3f386d42c19dd0f6f3e71
Ivan Vučica <ivan@vucica.net>
2010-10-18 02:01:59 +0200

    Cleaned up transmission of packets. Identifying incoming stanzas' namespace and name.

--- zxmpp_packet.js
+++ zxmpp_packet.js
@@ -2,7 +2,7 @@
  * Z-XMPP
  * A Javascript XMPP client.
  *
- * DOCUMENTATION ON BOSH: http://xmpp.org/extensions/xep-0124.html 
+ * (c) 2010 Ivan Vucica
  */
  
 zxmppClass.prototype.packet = function (zxmpp)
@@ -17,12 +17,17 @@ zxmppClass.prototype.packet = function (zxmpp)
 	// prefer setting them just before sending
 	this.xml_body = this.xml.firstChild;
 	this.xml_body.setAttribute('content','text/html; charset=utf-8');
+	this.xml_body.setAttribute('xmlns:xmpp','urn:xmpp:xbosh');
 
 	// parsed stanzas
 	this.iq = false;
 	this.message = false;
 	this.presence;
 	
+	// defined namespaces
+	// used only when parsing
+	this.namespaces = {};
+	this.defaultNamespace = "http://jabber.org/protocol/httpbind";
 	
 	/* functions */
 	this.addIq = function(idtype, type, to)
@@ -66,12 +71,95 @@ zxmppClass.prototype.packet = function (zxmpp)
 		var outxml = this.zxmpp.util.serializedXML(this.xml);
 		
 		// output to wire
-		this.zxmpp.stream.transmitPacket(outxml);
+		this.zxmpp.stream.transmitPacket(outxml, "poll");
 	}
 	
 	
 	this.parseXML = function(xml)
 	{
+		this.xml = xml;
 		
+		// root element, xml.firstChild, is <body>
+		this.xml_body = xml.firstChild;
+		
+		// we need to extract namespace specs from body
+		// just extract those with colon, since by default we
+		// presume xmlns to be http://jabber.org/protocol/httpbind
+		var attrs = this.zxmpp.util.easierAttrs(this.xml_body);
+		for(var attr in attrs)
+		{
+			var colonsplit = attr.split(":");
+			if(colonsplit[0]=="xmlns" && colonsplit[1])
+			{
+				this.namespaces[colonsplit[1]] = attrs[attr];
+				//console.log("namespace '" + colonsplit[1] + "': " + attrs[attr]);
+			}
+		}
+		
+		
+		// now find the stanzas in the body, and parse them
+		for(var i in this.xml_body.childNodes)
+		{
+			var child = this.xml_body.childNodes[i];
+			if(!child.nodeName) continue;
+			
+			var nsurl = false;
+			var stanza = false;
+			if(child.nodeName.split(":").length>1)
+			{
+				var ns = child.nodeName.split(":")[0];
+				this.zxmpp.util.easierAttrs(child);
+			
+				stanza = child.nodeName.split(":")[1];
+				
+				if(child.attr["xmlns:" + ns])
+				{
+					nsurl = child.attr["xmlns:" + ns];
+				}
+				else if(this.namespaces[ns])
+				{
+					nsurl = this.namespaces[ns];
+				}
+			}
+			else
+			{
+				stanza = child.nodeName;
+				nsurl = this.defaultNamespace;
+			}
+			
+			if(!nsurl)
+			{
+				console.warn("zxmpp::packet::parseXML(): Stanza " + child.nodeName + " in unknown namespace. Stanza dropped");
+				return;
+			}
+			
+			// now we have enough data about stanza to start parsing it
+			// let's iterate through supported stanzas!
+			console.log("zxmpp::packet::parseXML(): Stanza " + stanza + " in namespace " + nsurl);
+			var stanzaInstance = false;
+			switch(nsurl)
+			{
+				case "http://etherx.jabber.org/streams":
+				switch(stanza)
+				{
+					case "features":
+					stanzaInstance = new this.zxmpp.stanzaStreamFeatures(this.zxmpp);
+					break;
+				}
+				break;
+			
+			}
+			
+			if(!stanzaInstance)
+			{
+				console.warn("zxmpp::packet::parseXML(): Stanza \'" + stanza + "\' in namespace \'" + nsurl + "\' is unknown. Stanza dropped");
+			}
+			else
+			{
+				stanzaInstance.parseXML(child);
+			}
+			
+			
+		}
 	}
 }
---
+++ zxmpp_stanzastreamfeatures.js
@@ -0,0 +1,17 @@
+/* 
+ * Z-XMPP
+ * A Javascript XMPP client.
+ *
+ * (c) 2010 Ivan Vucica
+ */
+ 
+// handling of "features" in namespace "http://etherx.jabber.org/streams"
+zxmppClass.prototype.stanzaStreamFeatures = function(zxmpp)
+{
+	this.zxmpp = zxmpp;
+	
+	this.parseXML = function (xml)
+	{
+		
+	}
+}
