6a25d589095b5bdc269bf62100775d41ca71c57c
Ivan Vučica <ivan@vucica.net>
2010-10-20 02:41:00 +0200

    Mostly complete caps handling. Tried to work out a retry scheme to improve connection handling. Added iq failure and empty result handling.

--- zxmpp_caps.js
+++ zxmpp_caps.js
@@ -5,7 +5,8 @@
  * (c) 2010 Ivan Vucica
  */
  
-// represents caps of a single entity on xmpp network
+// typically represents caps of a single entity on xmpp network,
+// but can also be cached in a node
 zxmppClass.prototype.caps = function(zxmpp)
 {
 	this.zxmpp = zxmpp;
@@ -17,12 +18,18 @@ zxmppClass.prototype.caps = function(zxmpp)
 	
 	this.features = {};
 	this.ownerJid = false;
+	this.ownerNode = false;
 	
-	this.parseXML = function (xml)
+	this.nodeCategory = false;
+	this.nodeType = false;
+	this.nodeName = false;
+	
+	this.parseXML = function (xml) // parse "c"
 	{
 		
-		// style one:
+		// XEP-0115-3:
 		//	<c xmlns='http://jabber.org/protocol/caps' node='http://psi-im.org/caps' ver='caps-b75d8d2b25' ext='ca cs ep-notify html'/>
+		// latest XEP-0115-4 (as of oct 2010) also includes hash
 		
 		this.zxmpp.util.easierAttrs(xml);
 		
@@ -31,8 +38,48 @@ zxmppClass.prototype.caps = function(zxmpp)
 		this.ext = xml.attr["ext"];
 		this.hash = xml.attr["hash"];
 		
+		this.ownerNode = this.node + "#" + this.ver;
+		
 		// if we already know caps, skip
-		// TODO establish a client+ver database of caps
+		if(this.hash)
+		{
+			// XEP-0115-4
+			// we can use node database
+			
+			if(this.zxmpp.capsNodes[this.ownerNode + "#" + this.hash])
+			{
+				// replace this instance in its owner zxmpp::presence with
+				// the cached zxmpp::caps based on the hash
+				
+				// TODO must copy, not reference, and then replace ownerJid!
+				this.zxmpp.getPresence(this.ownerJid).caps = this.zxmpp.capsNodes[this.ownerNode + "#" + this.hash];
+				
+				return;
+			}
+		}
+		else
+		{
+			// XEP-0115-3 legacy format
+			// using database is impossible,
+			// since we cannot verify caps
+			
+			// TODO implement "ext" parsing
+			
+			// TODO decide if we will trust clients that dont serve us hash
+			// for now we will trust 
+			if(this.zxmpp.capsNodes[this.ownerNode])
+			{
+				// replace this instance in its owner zxmpp::presence with
+				// the cached zxmpp::caps, based on just the node+ver
+				
+				// TODO must copy, not reference, and then replace ownerJid!
+				this.zxmpp.getPresence(this.ownerJid).caps = this.zxmpp.capsNodes[this.ownerNode];
+				
+				return;
+			}
+
+		}
+
 		
 		// discover caps by sending disco info
 		if(!this.ownerJid)
@@ -45,17 +92,26 @@ zxmppClass.prototype.caps = function(zxmpp)
 		
 		
 		
-		/*
+	}
+
+	this.finishProcessing = function()
+	{
+		
+		// TODO must copy, not reference, and then replace ownerJid with 'false'!
+
 		if(this.hash)
 		{
-			// XEP-0115-4
+			// TODO add hash spoofing verification!
+			
+			this.zxmpp.capsNodes[this.ownerNode + "#" + this.hash] = this;
 		}
 		else
 		{
-			// XEP-0115-3 legacy format
-			// TODO unimplemented
-		}*/
-
+			// TODO decide if we should trust the client without a hash!
+			// let's still cache based on ver, and trust
+			
+			this.zxmpp.capsNodes[this.ownerNode] = this;
+		}
 	}
 
 };
