fafac25101e4f28393cb6c5cb264c396ff3f569e
Ivan Vučica <ivan@vucica.net>
2010-11-26 12:40:03 +0100

    index.php now redirects to ivan.vucica.net in case it's running on something other than ivan.vucica.net, else it redirects to bitbucket.

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<? header("Location: http://bitbucket.org/ivucica/zxmpp/"); ?>
+<?
+	if($_SERVER["SERVER_NAME"] != "ivan.vucica.net")
+	{
+		header("Location: http://ivan.vucica.net/zxmpp/");
+	}
+	else
+	{
+		header("Location: http://bitbucket.org/ivucica/zxmpp/"); 
+	}
+?>
